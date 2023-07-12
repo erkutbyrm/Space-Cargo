35df45800c5b7cc0d0d0225d7977d1ad3260e595
erkutbyrm <erkut_byrm@hotmail.com>
2023-07-12 17:03:58 +0300

    add asteroid destroy animation MAYADEM-13

--- Assets/Scripts/AsteroidCollisionSystem.cs
+++ Assets/Scripts/AsteroidCollisionSystem.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class AsteroidCollisionSystem : MonoBehaviour
 {
+    public GameObject explosionPrefab;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("SpaceShip"))
-        { gameObject.SetActive(false); }
+        { 
+            GameObject explosion = GameObject.Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 1f);
+            gameObject.SetActive(false);
+        }
     }
 }
